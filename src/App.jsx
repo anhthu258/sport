675fca9f2ac90cae4d6236e7b4a9b3d31d0778e1
Riiskager <emil.axel.riis@gmail.com>
2025-10-15 10:16:26 +0200

    commit, hub

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 // App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Discover from "./pages/Discover";
+import Map from "./pages/Map";
+import Login from "./pages/Login";
+import Hometest from "./pages/Hometest";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Hometest />} />
+        <Route path="/Discover" element={<Discover />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Map" element={<Map />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Hometest.jsx
@@ -0,0 +1,13 @@
+import Discover from "./pages/Discover";
+import Map from "./pages/Map";
+import Login from "./pages/Login";
+
+
+export default function Hometest(){
+    return(
+        <div>
+            <h1>her er hubben</h1>
+    
+        </div>
+    )
+}
