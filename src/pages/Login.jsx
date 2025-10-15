20d36a3c7cbd1a6842ed170d70cdcb86df94a1f5
anhthu258 <k.anhthu02@gmail.com>
2025-10-15 10:31:12 +0200

    login kort tilføjelse + temp file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,10 @@
-export default function Login(){
-    
+import LoginForm from '../components/LoginForm';
+
+export default function Login() {
+    return (
+        <div className="login-container">
+            <h2>Login</h2>  
+            <LoginForm />
+        </div>
+    );
 }
