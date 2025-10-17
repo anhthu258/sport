78d35f2d206900a97b05781ec4329c1674062b02
ankerderbanker <anker2000@hotmail.com>
2025-10-17 09:42:49 +0200

    Kortvirkerstyles

--- src/pages/Hometest.jsx
+++ src/pages/Hometest.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router";
 
-export default function Hometest(){
-    return(
-     <div>
+export default function Hometest() {
+  return (
+    <div>
       <h1>Her er hubben</h1>
       <nav>
         <ul>
-          <li><Link to="/map">Map</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/discover">Discover</Link></li>
+          <li>
+            <Link to="/map">Map</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/discover">Discover</Link>
+          </li>
         </ul>
       </nav>
     </div>
