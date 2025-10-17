11d6ae843b2629381c6882b106d86df3f0db5757
Annetteskou <Eaa24amsk@students.eaaa.dk>
2025-10-17 10:50:09 +0200

    Update Opret_post.jsx

--- src/pages/Opret_post.jsx
+++ src/pages/Opret_post.jsx
@@ -1,5 +1,5 @@
 //opsæt formular
-import { useState } from "react"
+import { useState } from "react";
 import { serverTimestamp } from "firebase/firestore";
 //import firebase elementer der skal bruges
 import { db } from "../assets/firebase";
@@ -8,72 +8,70 @@ import { collection, addDoc } from "firebase/firestore"; //importerer collection
 
 // import firebase fra hotspot
 
-export default function OpretPost(){
-    const [beskrivelse, setBeskrivelse] = useState(""); //Gemmer beskrivelsen, som brugeren skriver
-    const [sport, setSport] = useState(""); //gemmer sporten, som brugeren vælger, afhænger af hvad, der er muligt for hotspottet
-    const [message, setMessage] = useState(""); // Viser beskeder til brugeren
-    const [location, setLocation] = useState(""); //skal komme fra en bestemt liste man kan vælge imellem, eller fra hotspot_data
+export default function OpretPost() {
+  const [beskrivelse, setBeskrivelse] = useState(""); //Gemmer beskrivelsen, som brugeren skriver
+  const [sport, setSport] = useState(""); //gemmer sporten, som brugeren vælger, afhænger af hvad, der er muligt for hotspottet
+  const [message, setMessage] = useState(""); // Viser beskeder til brugeren
+  const [location, setLocation] = useState(""); //skal komme fra en bestemt liste man kan vælge imellem, eller fra hotspot_data
 
-    //backend af formular kommet her
+  //backend af formular kommet her
 
-    //async function "handleSubmit", der tjekker forskellige parametrer
-   
-    async function handleSubmit(e) {
+  //async function "handleSubmit", der tjekker forskellige parametrer
 
-  e.preventDefault(); // forhindrer side reload 
-    
-    if (!beskrivelse || !sport) { //Hvis der ikke er beskrivelse eller sport, så skriv det lige ind
-    setMessage("Udfyld venligst alle felter");
-    return;
-  }
+  async function handleSubmit(e) {
+    e.preventDefault(); // forhindrer side reload
 
-  console.log("Forsøger at oprette opslag:", { beskrivelse, sport, location });
+    if (!beskrivelse || !sport) {
+      //Hvis der ikke er beskrivelse eller sport, så skriv det lige ind
+      setMessage("Udfyld venligst alle felter");
+      return;
+    }
 
-await addDoc(collection(db, "posts"), { //sender til "db"=firestore, og direkte ind i posts, men først når den har
-  creatorId: "BrugerID",//alle de her ting ;)
-  hotspotId: location, //location skal bestemmes ud fra hotspottet man har valgt
-  beskrivelse, 
-  sport, //skal ændres ud fra også en select few sportgrene
-  timestamp: serverTimestamp() 
-});
+    console.log("Forsøger at oprette opslag:", {
+      beskrivelse,
+      sport,
+      location,
+    });
+
+    await addDoc(collection(db, "posts"), {
+      //sender til "db"=firestore, og direkte ind i posts, men først når den har
+      creatorId: "BrugerID", //alle de her ting ;)
+      hotspotId: location, //location skal bestemmes ud fra hotspottet man har valgt
+      beskrivelse,
+      sport, //skal ændres ud fra også en select few sportgrene
+      timestamp: serverTimestamp(),
+    });
 
     setMessage("Opslag oprettet!"); //Tømmer formularen
     setBeskrivelse("");
     setSport("");
     setLocation("");
-}
- 
+  }
 
-
-
-    return(
-        <div className="OpretPost">
-            <form onSubmit={handleSubmit}>
-                <label>Lokation</label>
-                <input 
-                    value={location} //ændres til dropdown menu senere, for nu bare tekst
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="Vælg lokation"
-                    
-                />
-                <label>Beskrivelse</label>
-                <input
-                    value={beskrivelse}
-                    onChange={(e) => setBeskrivelse(e.target.value)}
-                    placeholder="Skriv en kort titel"
-                />
-                <label>Sport</label>
-                <input
-                    value={sport} //I et andet dokument skal titel = sport
-                    onChange={(e) => setSport(e.target.value)} //sætter sporten til hvad man har valgt, det her skal ændres til "sport tags"
-                    placeholder="Sport fx basketball"
-                />
-                <button type="submit">Opret opslag</button>
-                {message && <p>{message}</p>}
-
-            </form>
-
-        </div>
-        
-    )
+  return (
+    <div className="OpretPost">
+      <form onSubmit={handleSubmit}>
+        <label>Lokation</label>
+        <input
+          value={location} //ændres til dropdown menu senere, for nu bare tekst
+          onChange={(e) => setLocation(e.target.value)}
+          placeholder="Vælg lokation"
+        />
+        <label>Beskrivelse</label>
+        <input
+          value={beskrivelse}
+          onChange={(e) => setBeskrivelse(e.target.value)}
+          placeholder="Skriv en kort titel"
+        />
+        <label>Sport</label>
+        <input
+          value={sport} //I et andet dokument skal titel = sport
+          onChange={(e) => setSport(e.target.value)} //sætter sporten til hvad man har valgt, det her skal ændres til "sport tags"
+          placeholder="Sport fx basketball"
+        />
+        <button type="submit">Opret opslag</button>
+        {message && <p>{message}</p>}
+      </form>
+    </div>
+  );
 }
