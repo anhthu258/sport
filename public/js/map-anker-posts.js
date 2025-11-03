6229a5fffe97fe2cc33e48ef3b0b3f1d1ef022e0
ankerderbanker <anker2000@hotmail.com>
2025-11-03 09:16:28 +0100

    delete duplicate

--- public/js/map-anker-posts.js
+++
@@ -1,9 +0,0 @@
-// Deprecated shim – this file existed with merge artifacts and is no longer used.
-// Please load "/js/map-anker-posts.clean.js" instead (already referenced in MapAnker.html).
-// Keeping this lightweight shim to avoid breaking any stray references.
-
-console.warn(
-  "[map-anker-posts.js] Loading implementation from map-anker-posts.clean.js"
-);
-import * as __impl from "./map-anker-posts.clean.js";
-export default __impl;
