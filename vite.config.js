33dabfab4b42aabbed6c1117339945ce12c1f148
anhthu258 <k.anhthu02@gmail.com>
2025-11-04 19:18:20 +0100

    gitpage pathing fix?

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/"
+    base: "/sport/"
   };
 
   // Change base path when building for production
